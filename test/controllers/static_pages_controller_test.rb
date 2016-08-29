af6fa434fa122568ebf1098757f084cf8a6b543b
Michael Mereos <mereosm@gmail.com>
2016-08-29 23:15:18 +1000

    added some addition dynamic pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -19,5 +20,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get contact page" do
+  get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 
 end
+
